c95fbbba467aba5b0d4000f55f90a9986af75326
Atsushi-Shinmokawa <158016786+Atsushi-Shinmokawa@users.noreply.github.com>
2026-01-16 01:58:25 +0900

    feat(reading-notes): add ReadingNoteService

---
+++ app/Services/ReadingNoteService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\ReadingLog;
+use App\Models\ReadingNote;
+
+class ReadingNoteService
+{
+    /**
+     * 読書ノートを作成
+     */
+    public function store(ReadingLog $readingLog, array $data): ReadingNote
+    {
+        return $readingLog->readingNotes()->create($data);
+    }
+
+    /**
+     * 読書ノートを削除
+     */
+    public function destroy(ReadingLog $readingLog, ReadingNote $readingNote): void
+    {
+        // 親子関係の整合性チェック
+        if ($readingNote->reading_log_id !== $readingLog->id) {
+            abort(404);
+        }
+
+        $readingNote->delete();
+    }
+}
