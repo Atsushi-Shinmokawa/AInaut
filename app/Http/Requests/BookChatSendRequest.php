91e5bde81b85819048021df06c848b808e10019f
Atsushi-Shinmokawa <158016786+Atsushi-Shinmokawa@users.noreply.github.com>
2026-01-20 00:09:04 +0900

    feat: add BookChatSendRequest for chat message validation

---
+++ app/Http/Requests/BookChatSendRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+class BookChatSendRequest extends AbstractFormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'content' => ['required', 'string', 'max:4000'],
+        ];
+    }
+}
